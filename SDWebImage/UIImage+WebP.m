50b04fb930470a1b04df1bd47f352880bed78927
hongjy <hjy322@hotmail.com>
2016-03-29 16:42:22 +0800

    Animated webp support.

--- SDWebImage/UIImage+WebP.m
+++ SDWebImage/UIImage+WebP.m
@@ -8,62 +8,31 @@
 
 #ifdef SD_WEBP
 #import "UIImage+WebP.h"
-
-#if !COCOAPODS
-#import "webp/decode.h"
-#else
-#import "libwebp/webp/decode.h"
-#endif
-
-// Callback for CGDataProviderRelease
-static void FreeImageData(void *info, const void *data, size_t size)
-{
-    free((void *)data);
-}
+#import <YYImage/YYImageCoder.h>
 
 @implementation UIImage (WebP)
 
 + (UIImage *)sd_imageWithWebPData:(NSData *)data {
-    WebPDecoderConfig config;
-    if (!WebPInitDecoderConfig(&config)) {
-        return nil;
+    @autoreleasepool {
+        YYImageDecoder *decoder = [YYImageDecoder decoderWithData:data scale:[UIScreen mainScreen].scale];
+        UIImage *image;
+        if (decoder.frameCount > 1) {
+            NSMutableArray *images = [NSMutableArray new];
+            CGFloat duration = 0;
+            for (int i = 0; i < decoder.frameCount; ++i) {
+                UIImage *image = [decoder frameAtIndex:i decodeForDisplay:YES].image;
+                if (image != nil) {
+                    [images addObject:image];
+                    CGFloat frameDuration = [decoder frameDurationAtIndex:i];
+                    duration += frameDuration > 1e-3 ? frameDuration : 0.15;
+                }
+            }
+            image = [UIImage animatedImageWithImages:images duration:duration];
+        } else {
+            image = [decoder frameAtIndex:0 decodeForDisplay:YES].image;
+        }
+        return image;
     }
-
-    if (WebPGetFeatures(data.bytes, data.length, &config.input) != VP8_STATUS_OK) {
-        return nil;
-    }
-
-    config.output.colorspace = config.input.has_alpha ? MODE_rgbA : MODE_RGB;
-    config.options.use_threads = 1;
-
-    // Decode the WebP image data into a RGBA value array.
-    if (WebPDecode(data.bytes, data.length, &config) != VP8_STATUS_OK) {
-        return nil;
-    }
-
-    int width = config.input.width;
-    int height = config.input.height;
-    if (config.options.use_scaling) {
-        width = config.options.scaled_width;
-        height = config.options.scaled_height;
-    }
-
-    // Construct a UIImage from the decoded RGBA value array.
-    CGDataProviderRef provider =
-    CGDataProviderCreateWithData(NULL, config.output.u.RGBA.rgba, config.output.u.RGBA.size, FreeImageData);
-    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
-    CGBitmapInfo bitmapInfo = config.input.has_alpha ? kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast : 0;
-    size_t components = config.input.has_alpha ? 4 : 3;
-    CGColorRenderingIntent renderingIntent = kCGRenderingIntentDefault;
-    CGImageRef imageRef = CGImageCreate(width, height, 8, components * 8, components * width, colorSpaceRef, bitmapInfo, provider, NULL, NO, renderingIntent);
-
-    CGColorSpaceRelease(colorSpaceRef);
-    CGDataProviderRelease(provider);
-
-    UIImage *image = [[UIImage alloc] initWithCGImage:imageRef];
-    CGImageRelease(imageRef);
-
-    return image;
 }
 
 @end
